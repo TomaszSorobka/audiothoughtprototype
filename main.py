ece4fc2862a612566077e5cd8d5e50250c1012d5
TomaszSorobka <tomek.sorobka@gmail.com>
2023-09-02 15:53:07 +0200

    snall fix

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ title = """
 """
 st.markdown(title, unsafe_allow_html=True)
 guide = '''
-<h4>Please allow for the use of microphone! Use a bro</h4>
+<h4>Please allow for the use of microphone!</h4>
 1. Press the grey microphone below to start recording <br>  2. Speak your thoughts (max. 3 minutes) <br> 3. Press the red microphone icon to stop recording <br> 4. Wait for the summary to generate! <br><br>
 '''
 st.write(guide, unsafe_allow_html=True)
